f949510dad60f1c4d9746789f8355c57024c4cde
Linklab <link@lab.edu>
2019-04-19 11:12:58 -0400

    new code

--- src/LotFollower.py
+++ src/LotFollower.py
@@ -29,7 +29,7 @@ class LotFollower():
         self.velocity_publisher.publish(self.vel_msg)
 
     def turn_left(self):
-        self.vel_msg.angular.z = 10
+        self.vel_msg.angular.z = 14
         t0 = rospy.Time.now().to_sec()
         current_distance = 0
         self.velocity_publisher.publish(self.vel_msg)
@@ -40,14 +40,55 @@ class LotFollower():
         self.vel_msg.angular.z = 0
         self.velocity_publisher.publish(self.vel_msg)
 
-    def goto_parking_spot(self, spot_number):
-        if (spot_number == 2):
-            self.move_forward(28)
-            self.turn_left()
-            self.move_forward(20)
+    
+    def turn_right(self):
+        self.vel_msg.angular.z = -14
+        t0 = rospy.Time.now().to_sec()
+        current_distance = 0
+        self.velocity_publisher.publish(self.vel_msg)
+        while(current_distance < 8):
+            self.velocity_publisher.publish(self.vel_msg)
+            t1 = rospy.Time.now().to_sec()
+            current_distance = 19* (t1-t0)
+        self.vel_msg.angular.z = 0
+        self.velocity_publisher.publish(self.vel_msg)
 
+        
+    def goto_parking_spot(self, spot_number):
+        if (spot_number == 1):
+            self.move_forward(25.5)
+            self.turn_left()
+            self.move_forward(15)
+            self.turn_right()
+            #now needs to return home
+            self.turn_right()
+            self.move_forward(15)
+            self.turn_right()
+            self.move_forward(25.5)
+            self.turn_left()
+            self.turn_left()
+            #end of return home code --- has not been tested yet
+            
+        elif (spot_number == 2):
+            self.move_forward(25.5)
+            self.turn_left()
+            self.move_forward(15)
+            self.turn_left()
+            
+
+        elif (spot_number == 3):
+            self.move_forward(25.5)
+            self.turn_left()
+            self.move_forward(29)
+            self.turn_right()
+            
+        elif (spot_number == 4):
+            self.move_forward(25.5)
+            self.turn_left()
+            self.move_forward(29)
+            self.turn_left()
 
 
 if __name__=='__main__':
     lf = LotFollower()
-    lf.goto_parking_spot(2)
+    lf.goto_parking_spot(1)
